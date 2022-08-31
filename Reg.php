7782d7f6f6a1e94e5fda12cef88f97157260b19e
Junaid Ahmad <112062309+qjunaid@users.noreply.github.com>
2022-08-31 12:58:11 +0530

    Add files via upload

---
+++ Reg.php
@@ -0,0 +1,28 @@
+<html>
+<head><meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet"  href="reg.css">
+<title>
+Registration Form
+</title>
+</head>
+<body>
+<div class="div1"><br><br>
+Registration<br><br><br>
+ <input type="radio" name="radio1"> Personal
+<input type="radio" name="radio1">Company
+ <form>
+<img src="e.png" width="25" height="25">
+<input type="text" name="txt1" placeholder="Email">  <br><br>
+<img src="n.png" width="25" height="25">
+<input type="text" name="txt2" placeholder="Name">  <br><br>
+<img src="p.png" width="25" height="25">
+<input type="text" name="txt3" placeholder="Password"> <br><br>
+<input type="radio" name="radio3">Male 
+<input type="radio" name="radio4">Female
+</form>
+By clicking Register, you agree<br> on our<a href="">Terms & Conditions.</a>
+<input type="submit" value="Register" id="btn">
+</div>
+</body>
+</html>
