9af260d6276fcee21c9b6ccc227a53a35e71f197
Junaid Ahmad <112062309+qjunaid@users.noreply.github.com>
2022-09-21 21:37:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
  ReactDOM.render(
-<h1>Hello World.</h1>,
+<h1>Hello World...</h1>,
 <p>Welcome.</p>
 <h2>Reactjs</h2>
 <b>JavaScript</b>
