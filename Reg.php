13f953b0d6daede56112e64168c269247a49fde7
Junaid Ahmad <112062309+qjunaid@users.noreply.github.com>
2022-09-29 18:19:36 +0530

    Update Reg.php

--- Reg.php
+++ Reg.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet"  href="reg.css">
 <title>
-Registration Form
+Registration Form...
 </title>
 </head>
 <body>
